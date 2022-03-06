6403e7925d1b49190da1c61f4882ac2c4e02f54a
sbs1621 <qudtjr4218@gmail.com>
2022-03-06 15:16:41 +0900

    What is MVC?

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,8 +1,10 @@
 package hello.hellospring.controller;
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -13,5 +15,8 @@ public class HelloController {
         return "hello";
     }
     @GetMapping("hello-mvc")
-    pubilc String hello 
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
 }
