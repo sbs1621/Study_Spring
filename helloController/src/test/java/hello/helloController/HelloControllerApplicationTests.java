84badac5fa5096350242b9623c8b9a45aaf335ae
sbs1621 <qudtjr4218@gmail.com>
2022-03-10 19:21:30 +0900

    createStudyProject

---
+++ helloController/src/test/java/hello/helloController/HelloControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.helloController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
